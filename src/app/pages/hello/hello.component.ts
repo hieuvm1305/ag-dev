98593b36cf24287c981b54d5ca905ecfcd1b13d2
hieuvm1305 <Hieuvm1305@gmail.com>
2024-12-20 14:37:29 +0700

    test

--- src/app/pages/hello/hello.component.ts
+++ src/app/pages/hello/hello.component.ts
@@ -10,7 +10,7 @@ import { GlobalModules } from '../../global.module';
 export class HelloComponent {
   name: string = 'John'
   counter = 1
-  text = 'Hi'
+  text = 'Hi2'
   increase() {
     this.counter += 1
   }
